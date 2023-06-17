9bd02e2de7168b5dfcef23d6b590f90735fbe90b
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-06-16 21:05:25 -0700

    update and fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CsvHelper;
+using MyClassLibary;
+
 
 namespace CSVs___Writing_Reading_and_Preloading
 {
@@ -20,9 +15,68 @@ namespace CSVs___Writing_Reading_and_Preloading
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string filePath = "students.csv";
+        List<Student> loadedStudents =new List<Student>();
         public MainWindow()
         {
             InitializeComponent();
+    
+
+            using (StreamReader sr = new StreamReader(filePath))
+            using (CsvReader csv = new CsvReader(sr, CultureInfo.InvariantCulture))
+            {
+
+                loadedStudents = csv.GetRecords<Student>().ToList();
+            }
+
+            lvStudents.ItemsSource = loadedStudents;
+
+        }
+
+
+        public void Preload()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student( "Will", "Cram", 57, 93),
+                new Student( "Josh", "Emery", 101, 105)
+            };
+
+            SaveCSVFile(filePath, students);
+        }
+
+        public void SaveCSVFile<T>(string filePath, List<T> list)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+       
+
+            using (var stream = File.Open(filePath, FileMode.OpenOrCreate))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    using (var csvWriter = new CsvWriter(writer, ci))
+                    {
+                        csvWriter.WriteRecords(list);
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+            int csi = int.Parse(txtCSI.Text);
+            int genEd = int.Parse(txtGenEn.Text);
+
+            loadedStudents.Add(new Student( firstName, lastName, csi, genEd));
+            lvStudents.Items.Refresh();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveCSVFile(filePath, loadedStudents);
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLibary;
+
+namespace CSVs___Writing_Reading_and_Preloading
+{
+    public class Student
+    {
+        string _firstName;
+        string _lastName;
+        int csiGrade;
+        int genEdGrade;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string firstName, string lastName, int csiGrade, int genEdGrade)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            this.csiGrade = csiGrade;
+            this.genEdGrade = genEdGrade;
+        }
+
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public int CsiGrade { get => csiGrade; set => csiGrade = value; }
+        public int GenEdGrade { get => genEdGrade; set => genEdGrade = value; }
+        public double Average { get => (CsiGrade + GenEdGrade) / 2; }
+    }
+
+
+    
+}
